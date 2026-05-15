2393f5e6959378c1e1fd4d991bb9bea443aaabb0
Ludmila92 <baraulja92@gmail.com>
2026-05-15 20:56:35 +0200

    Step 2 - Food choise

--- code/script.js
+++ code/script.js
@@ -10,7 +10,17 @@ const userName = prompt(`Great! First, what's your name?`);
 alert(`Hi ${userName}! Let's get started with your order.`);
 
 // Step 2 - Food choice
-// Your code goes here
+const foodChoise = prompt(
+  `What would you like to order? Please choose one of the following options. \n1. Pizza \n2. Pasta \n3. Salad`,
+);
+
+if (foodChoise === "1") {
+  alert(`You chose Pizza! Great choise!`);
+} else if (foodChoise === "2") {
+  alert(`You chose Pasta! Great choise!`);
+} else {
+  alert(`You chose Salad! Great choise!`);
+}
 
 // Step 3 - Subtype choice
 // Your code goes here
